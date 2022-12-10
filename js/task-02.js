6fc2c1def4e2acbed366bc605d4c4f4f204df30b
Dawid Bazyluk <dawid.bazyluk@gmail.com>
2022-12-10 16:19:51 +0100

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsList = document.querySelector("#ingredients");
+
+ingredients.forEach((ingredient) => {
+  const element = document.createElement("li");
+  element.textContent = ingredient;
+  ingredientsList.append(element);
+  element.classList.add("item");
+});
