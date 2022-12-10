65f95b3d3f2fb63358f148a7e510420af1b08f1c
Dawid Bazyluk <dawid.bazyluk@gmail.com>
2022-12-10 15:04:27 +0100

    start

---
+++ js/task-01.js
@@ -0,0 +1,23 @@
+const itemsByCategory = document.querySelectorAll(".item");
+const countCategories = `Number of categories: ${itemsByCategory.length}`;
+const title = document.querySelectorAll("h2").innerHTML;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*----------------------------*/
+console.log(countCategories);
+
