64f281de997e76ff2402c1fdb88d804832cbe679
Dawid Bazyluk <dawid.bazyluk@gmail.com>
2022-12-12 17:46:15 +0100

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,8 +1,11 @@
 const slider = document.querySelector("#font-size-control");
 const text = document.querySelector("#text");
 
+text.style.fontSize = slider.value + "px";
+
 const changeSize = () => {
-  text.style.fontSize = slider.value + "px";
+  const currentSize = slider.value;
+  text.style.fontSize = currentSize + "px";
 };
 
 slider.addEventListener("input", changeSize);
